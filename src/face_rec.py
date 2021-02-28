b312ceb52e819ac47dfe45db26b229804e25b8e3
mohammadawwad <mohd-awwad@hotmail.com>
2021-02-28 12:34:41 -0800

    face detection working , there is a bug to fix , and need to add mask detection

--- src/face_rec.py
+++ src/face_rec.py
@@ -2,8 +2,17 @@ import os
 import numpy as np 
 import cv2 
 from datetime import datetime
+import pickle
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt2.xml")
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("src/trainner/face_trainner.yml")
+
+labels = {"persons_name" : 1}
+with open("labels.pickle", "rb") as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
+
 cap = cv2.VideoCapture(1)
 
 time = datetime.now()
@@ -107,16 +116,31 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
-    
+
     for(x ,y, h, w) in faces:
         print(x ,y, h, w)
 
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-        img_item = "images/mohammad.png"
+
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 45:
+            print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (0, 0, 0) #Black
+            stroke = 2
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+
+        img_item = "images/recognized.png"
         cv2.imwrite(img_item, roi_gray)
         
-        face_color = (255, 0, 0) #BGR
+        #BGR
+        face_color = (255, 0, 0) #Blue
+        mask_true = (0, 255, 0)  #Green
+        mask_false = (0, 0, 255) #Red
+
         stroke = 2
         end_cord_x = x + w
         end_cord_y = y + h
---
+++ src/mask_trainer.py
@@ -0,0 +1,54 @@
+import os
+from PIL import Image
+import numpy as np
+import cv2
+import pickle
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+img_dir = os.path.join(base_dir, "mask")
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt2.xml")
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+current_id = 0
+mask_id = {}
+y_mask = []
+x_train = []
+
+for root, dirs, files in os.walk(img_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            label = os.path.basename(root).replace(" ", "-").lower()
+            #print(label, path)
+
+            if not label in mask_id:
+                mask_id[label] = current_id
+                current_id += 1
+
+            id_m = mask_id[label]
+            print(mask_id)
+            #y_labels.append(label)
+            #x_train.append(path)
+            pil_img = Image.open(path).convert("L")#gray scale
+            size = (550, 550)
+            final_img = pil_img.resize(size, Image.ANTIALIAS)
+            img_array = np.array(pil_img)
+            
+            #print(img_array)
+
+            faces = face_cascade.detectMultiScale(img_array, scaleFactor = 1.5, minNeighbors = 5)
+            for(x, y, h, w) in faces:
+                roi = img_array[y:y+h, x:x+w]
+                x_train.append(roi)
+                y_mask.append(id_m)
+
+
+#print(y_labels)
+#print(x_train)
+
+with open("labels.pickle", "wb") as f:
+    pickle.dump(mask_id, f)
+
+recognizer.train(x_train, np.array(y_mask))
+recognizer.save("src/trainner/mask_trainner.yml")
