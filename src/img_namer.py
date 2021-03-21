65e35a6b91f8201d664f5ad7e02c0aeff5019caa
mohammadawwad <mohd-awwad@hotmail.com>
2021-03-20 17:04:59 -0700

    updated file auto namer

--- src/img_namer.py
+++ src/img_namer.py
@@ -1,10 +1,13 @@
 #this is used to quickly and effiecently rename any files in a folder
-
 import os
 
-path = '/Users/Mohammad/Documents/GitHub/Python-Vision-Processing/src/mask/a'
+cd = input('what is the folder location: ')
+name = input('what would you like the file names to be: ')
+file_type = input('file type: ')
+
+path = '/Users/Mohammad/Documents/GitHub/Python-Vision-Processing/src/' + cd
 files = os.listdir(path)
 
 
 for index, file in enumerate(files):
-    os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(index), '.jpg'])))
+    os.rename(os.path.join(path, file), os.path.join(path, ''.join([name + '_' + str(index), file_type.lower()])))
